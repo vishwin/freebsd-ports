1dae8e4d2b29467c5f7f4f6a260e7128e508542b
Charlie Li <vishwin@FreeBSD.org>
2022-02-13 12:39:55 -0500

    ports-mgmt/poudriere-devel: add additional programs to make.nxb.conf

--- ports-mgmt/poudriere-devel/files/patch-src_share_poudriere_common.sh
+++ ports-mgmt/poudriere-devel/files/patch-src_share_poudriere_common.sh
@@ -1,8 +1,8 @@
 diff --git src/share/poudriere/common.sh src/share/poudriere/common.sh
-index 9e996f47..999002cf 100644
+index af13d175..5f9972a0 100755
 --- src/share/poudriere/common.sh
 +++ src/share/poudriere/common.sh
-@@ -2478,9 +2478,6 @@ setup_xdev() {
+@@ -2742,9 +2742,6 @@ setup_xdev() {
  
  	msg_n "Setting up native-xtools environment in jail..."
  	cat > "${mnt}/etc/make.nxb.conf" <<-EOF
@@ -12,7 +12,17 @@ index 9e996f47..999002cf 100644
  	NM=/nxb-bin/usr/bin/nm
  	LD=/nxb-bin/usr/bin/ld
  	OBJCOPY=/nxb-bin/usr/bin/objcopy
-@@ -2511,7 +2508,7 @@ setup_xdev() {
+@@ -2757,6 +2754,9 @@ setup_xdev() {
+ 	STRINGS=/nxb-bin/usr/bin/strings
+ 	AWK=/nxb-bin/usr/bin/awk
+ 	FLEX=/nxb-bin/usr/bin/flex
++	FIND=/nxb-bin/usr/bin/find
++	GREP=/nxb-bin/usr/bin/grep
++	XARGS=/nxb-bin/usr/bin/xargs
+ 	EOF
+ 
+ 	# as(1) has been removed in FreeBSD 13.0.  Just check if it's present
+@@ -2775,7 +2775,7 @@ setup_xdev() {
  			usr/bin/find usr/bin/gzcat usr/bin/awk \
  			usr/bin/touch usr/bin/sed usr/bin/patch \
  			usr/bin/install usr/bin/gunzip \
