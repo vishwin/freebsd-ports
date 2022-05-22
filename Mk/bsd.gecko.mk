db797d1dc6ce14e5af1814aad118a1341163ffff
Charlie Li <vishwin@FreeBSD.org>
2022-05-22 15:30:06 -0400

    Mk/bsd.gecko.mk: rework/update WASI logic

--- Mk/bsd.gecko.mk
+++ Mk/bsd.gecko.mk
@@ -95,7 +95,13 @@ MOZ_OPTIONS+=	--with-libclang-path="${LOCALBASE}/llvm${LLVM_DEFAULT}/lib"
 .    if !exists(/usr/bin/llvm-objdump)
 MOZ_EXPORT+=	LLVM_OBJDUMP="${LOCALBASE}/bin/llvm-objdump${LLVM_DEFAULT}"
 .    endif
-LLVM_VERSION=	14.0.2 # keep in sync with devel/wasi-compiler-rt${LLVM_DEFAULT}
+# Keep in sync with devel/wasi-* ports
+WASI_LLVM_DEFAULT=	14
+WASI_LLVM_VERSION=	14.0.2
+# Ignore Mk/bsd.default-versions.mk but respect make.conf(5) unless LTO is enabled
+.    if !defined(DEFAULT_VERSIONS) || ! ${DEFAULT_VERSIONS:Mllvm*} || ${PORT_OPTIONS:MLTO}
+LLVM_DEFAULT=	${WASI_LLVM_DEFAULT} # chase devel/wasi-* and bundled LLVM in lang/rust for LTO
+.    endif
 # Require newer Clang than what's in base system unless user opted out
 .    if ${CC} == cc && ${CXX} == c++ && exists(/usr/lib/libc++.so)
 BUILD_DEPENDS+=	${LOCALBASE}/bin/clang${LLVM_DEFAULT}:devel/llvm${LLVM_DEFAULT}
